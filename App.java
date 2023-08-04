8f325ceea70a29188a682941abb03a3deb89be5d
jaovg5 <140764535+jaovg5@users.noreply.github.com>
2023-08-04 17:27:11 -0300

    Update App.java

--- App.java
+++ App.java
@@ -6,9 +6,9 @@ package Atividade;
 public class App {
     public static void main(String[] args) {
       App aplicacao = new App();
-      Pessoa jog= new Pessoa("ff");
-      Piloto per1= new Piloto ("g");
-      Equipes grup= new Equipes("hh");
-      Mecanicos per2= new Mecanicos("f");
+      Pessoa jog= new Pessoa("name");
+      Piloto per1= new Piloto ("name");
+      Equipes grup= new Equipes("grupo");
+      Mecanicos per2= new Mecanicos("name");
     }
 }
